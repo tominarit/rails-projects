70743a6f979770ba9b11ad25fe45c2fcca5babf9
tominari <tominari@hitokuse.com>
2017-09-02 23:56:32 +0900

    added article model

---
+++ alpha_blog/db/migrate/20170902145310_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :articles do |t|
+      t.string   "title"
+      t.text     "description"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.timestamps
+    end
+  end
+end
