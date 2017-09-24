e64da9d27465d594ce0cb34314d9bf27dfe5f09b
tominari <tominari@hitokuse.com>
2017-09-24 22:37:29 +0900

    added login and logout functionality

--- alpha_blog/app/controllers/users_controller.rb
+++ alpha_blog/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -19,11 +20,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your Account Was Updated Successfuly"
       redirect_to articles_path
@@ -33,7 +33,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -42,4 +41,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
   
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
