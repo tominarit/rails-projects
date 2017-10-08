7b0d6407130f5c3ebdd38fa28b7d2c5e2bb7b112
tominari <tominari@hitokuse.com>
2017-10-08 11:50:58 +0900

    added user and stock association and stock restrictions

--- finance_tracker/config/routes.rb
+++ finance_tracker/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :user_stocks, except: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
